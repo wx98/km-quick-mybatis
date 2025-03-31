37c8eeee658a966ac7f59e6f800deaf7dba8d0cd
wx98 <wx.98@qq.com>
2025-03-31 20:40:18 +0800

    实现 Java 跳转到 MyBatis XML

--- src/main/java/cn/wx1998/kmerit/intellij/plugins/quickmybatis/dom/Mapper.java
+++ src/main/java/cn/wx1998/kmerit/intellij/plugins/quickmybatis/dom/Mapper.java
@@ -4,12 +4,50 @@ import com.intellij.psi.PsiClass;
 import com.intellij.util.xml.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 @Namespace("MybatisXml")
 public interface Mapper extends DomElement {
+
+    @NotNull
+    @SubTagsList({"insert", "update", "delete", "select"})
+    List<IdDomElement> getDaoElements();
 
     @Required
     @NameValue
     @NotNull
     @Attribute("namespace")
     GenericAttributeValue<PsiClass> getNamespace();
+
+    @NotNull
+    @SubTagList("sql")
+    List<Sql> getSqls();
+
+    @NotNull
+    @SubTagList("insert")
+    List<Insert> getInserts();
+
+    @NotNull
+    @SubTagList("update")
+    List<Update> getUpdates();
+
+    @NotNull
+    @SubTagList("delete")
+    List<Delete> getDeletes();
+
+    @NotNull
+    @SubTagList("select")
+    List<Select> getSelects();
+
+    @SubTagList("select")
+    Select addSelect();
+
+    @SubTagList("update")
+    Update addUpdate();
+
+    @SubTagList("insert")
+    Insert addInsert();
+
+    @SubTagList("delete")
+    Delete addDelete();
 }
